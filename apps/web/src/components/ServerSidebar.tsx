8318302a1ea11a9c778ecd9bbcc07330cca9b834
SnowFlake_Playzz <lmast@student.lentiz.nl>
2026-05-30 12:05:54 +0200

    Create ServerSidebar.tsx

---
+++ apps/web/src/components/ServerSidebar.tsx
@@ -0,0 +1,21 @@
+export default function ServerSidebar() {
+
+  return (
+
+    <aside
+      className="server-sidebar"
+    >
+
+      <button>
+        +
+      </button>
+
+      <button>
+        ❄️
+      </button>
+
+    </aside>
+
+  );
+
+}
