39ddc446fa21a51b1495bf865b769f609a809cf7
SnowFlake_Playzz <lmast@student.lentiz.nl>
2026-05-30 12:06:24 +0200

    Create ChannelSidebar.tsx

---
+++ apps/web/src/components/ChannelSidebar.tsx
@@ -0,0 +1,27 @@
+export default function ChannelSidebar() {
+
+  return (
+
+    <aside
+      className="channel-sidebar"
+    >
+
+      <h3>
+        SnowChat
+      </h3>
+
+      <ul>
+
+        <li># general</li>
+
+        <li># gaming</li>
+
+        <li># coding</li>
+
+      </ul>
+
+    </aside>
+
+  );
+
+}
